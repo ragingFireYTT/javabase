4084b79a803bf48a1bef99fe86aca40fc6e16412
ytt <929666130@qq.com>
2018-12-20 09:46:59 +0800

    插入排序

--- src/main/java/com/ytt/suanfa/sort/Sort.java
+++ src/main/java/com/ytt/suanfa/sort/Sort.java
@@ -28,8 +28,11 @@ public class Sort {
             System.out.println();
         }
     }
+    //冒泡 a[0]=1   a[1]=4   a[2]=4   a[3]=4   a[4]=5   a[5]=5   a[6]=5   a[7]=5   a[8]=8   a[9]=9   a[10]=11   a[11]=12   a[12]=12   a[13]=17   a[14]=22   a[15]=23   a[16]=25   a[17]=34   a[18]=44   a[19]=51   a[20]=78
+    //快速 a[0]=1   a[1]=4   a[2]=4   a[3]=4   a[4]=5   a[5]=5   a[6]=5   a[7]=5   a[8]=8   a[9]=9   a[10]=11   a[11]=12   a[12]=12   a[13]=17   a[14]=22   a[15]=23   a[16]=25   a[17]=34   a[18]=44   a[19]=51   a[20]=78
+    //插入 a[0]=1   a[1]=4   a[2]=4   a[3]=4   a[4]=5   a[5]=5   a[6]=5   a[7]=5   a[8]=8   a[9]=9   a[10]=11   a[11]=12   a[12]=12   a[13]=17   a[14]=22   a[15]=23   a[16]=25   a[17]=34   a[18]=44   a[19]=51   a[20]=78
 
-    //region 冒泡
+    //region 冒泡。 冒出最大的或者最小的到一端。
     // 冒，一轮，取出一个极值，放到一端
     @Test
     public void testMaoPao() {
@@ -49,11 +52,8 @@ public class Sort {
     }
     //endregion
 
-    //region 快速排序
+    //region 快速排序。  分区，小<= 基准 <= 大
     //小<= 基准 <= 大。 找到需要交换的一对 。 递归 (qian < hou)。
-    //冒泡 a[0]=1   a[1]=4   a[2]=4   a[3]=4   a[4]=5   a[5]=5   a[6]=5   a[7]=5   a[8]=8   a[9]=9   a[10]=11   a[11]=12   a[12]=12   a[13]=17   a[14]=22   a[15]=23   a[16]=25   a[17]=34   a[18]=44   a[19]=51   a[20]=78
-    //快速 a[0]=1   a[1]=4   a[2]=4   a[3]=4   a[4]=5   a[5]=5   a[6]=5   a[7]=5   a[8]=8   a[9]=9   a[10]=11   a[11]=12   a[12]=12   a[13]=17   a[14]=22   a[15]=23   a[16]=25   a[17]=34   a[18]=44   a[19]=51   a[20]=78
-
     @Test
     public void testQuickSort() {
         quickSort(0, arr.length - 1);
@@ -94,6 +94,29 @@ public class Sort {
         }
         if (qian < end) {
             quickSort(qian + 1, end); // 大分区迭代
+        }
+    }
+    //endregion
+
+    //region 插入法。   认为前面是有序的。 将后面的依次插入到前面有序的数组。
+    @Test
+    public void testChaRuSort(){
+        chaRuSort();
+        Tool.printA(arr);
+    }
+    public void chaRuSort(){
+         //lun 轮数 。
+        for (int lun = 0; lun < arr.length -1; lun++) {
+            // 比较次数  2轮 两个排好序
+            for (int biJiaoCiShu = 0; biJiaoCiShu < lun+1; biJiaoCiShu++) {
+                if (arr[lun+1- biJiaoCiShu] < arr [lun- biJiaoCiShu]) {
+                    int tmp = arr[lun + 1 -biJiaoCiShu];
+                    arr[lun+1-biJiaoCiShu] = arr[lun-biJiaoCiShu];
+                    arr[lun-biJiaoCiShu] = tmp;
+                }else {
+                    break;
+                }
+            }
         }
     }
     //endregion
