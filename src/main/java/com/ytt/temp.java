1d784d087f3eea38ae1f9e7967c644562f8d202c
ytt <929666130@qq.com>
2018-12-25 19:56:24 +0800

    --

---
+++ src/main/java/com/ytt/temp.java
@@ -0,0 +1,13 @@
+package com.ytt;
+
+/**
+ * Created by ytt on 2018/12/20.
+ */
+public class temp {
+    public static void main(String[] args) {
+        Integer i = 100;
+        System.out.println(i.hashCode());
+        Object o = new Object();
+        o.hashCode();
+    }
+}
